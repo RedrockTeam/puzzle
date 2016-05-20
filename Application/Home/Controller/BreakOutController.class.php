5f5b9a7fa8fa14f2862c223a712975cd44c19712
ztao <mail@vekwu.com>
2016-05-20 09:55:12 +0800

    突出重围微信验证和修了一个bug

--- Application/Home/Controller/BreakOutController.class.php
+++ Application/Home/Controller/BreakOutController.class.php
@@ -15,6 +15,60 @@ use Think\Exception;
 
 class BreakOutController extends Controller{
 
+    /*
+     * 用户同意授权，获取code
+     * 请求参数
+     */
+    private $appId = 'appid'; //公众号的唯一标识
+    private $state = 'auth'; //重定向后会带上state参数，开发者可以填写a-zA-Z0-9的参数值，最多128字节
+    private $responseType = 'code'; //返回类型，请填写code
+    private $scope = 'snsapi_userinfo'; //应用授权作用域，snsapi_base （不弹出授权页面，直接跳转，只能获取用户openid），snsapi_userinfo （弹出授权页面，可通过openid拿到昵称、性别、所在地。并且，即使在未关注的情况下，只要用户授权，也能获取其信息）
+    private $redirectUrl = 'http://hongyan.cqupt.edu.cn/puzzle/index.php/Hone/BreakOut/authCallback'; //授权后重定向的回调链接地址，请使用urlencode对链接进行处理
+    private $authtUrl = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=%s&redirect_uri=%s&response_type=%s&scope=%s&state=%s#wechat_redirect'; //微信授权地址
+
+    /* 请求成功后重定向到以下地址
+     * redirect_uri/?code=CODE&state=STATE。
+     */
+
+    /*
+     * 取得code后发起请求获得access_token
+     * 请求参数
+     * appid 上面定义
+     */
+    private $secret = 'ss'; //公众号的appsecret
+    private $grantType = 'authorization_code'; //填写为authorization_code
+    private $authTokenUrl = 'https://api.weixin.qq.com/sns/oauth2/access_token?appid=%s&secret=%s&code=%s&grant_type=%s';
+
+    /*
+     * 返回结果 具体参见 see to:https://mp.weixin.qq.com/wiki?t=resource/res_main&id=mp1421140842&token=&lang=zh_CN
+     */
+    /*正确
+     { "access_token":"ACCESS_TOKEN",
+    "expires_in":7200,
+    "refresh_token":"REFRESH_TOKEN",
+    "openid":"OPENID",
+    "scope":"SCOPE" }
+    错误
+    {"errcode":40029,"errmsg":"invalid code"}
+    */
+
+    /*
+     * 通过access_token获取用户信息
+     */
+
+    public function index(){
+        $this->antiCheat();
+        $this->display();
+    }
+
+    public function authCallback(){
+        $code = I('get.code');
+        if(empty($code)){
+            echo 'what are you doing';
+        }
+        $this->weChatAuth($code);
+    }
+
     public function submitScore(){
         $jsonStr = file_get_contents('php://input');
         @$json = json_decode($jsonStr, true);
@@ -43,7 +97,7 @@ class BreakOutController extends Controller{
                 $this->ajaxReturn($response);
             }
         }
-        if(empty($json['use_time']) || empty($json['barrier'])){
+        if(is_numeric(intval($json['use_time'])) || is_numeric(intval($json['barrier']))){
             $response = array(
                 'code' => 4,
                 'msg' => 'illegal param'
@@ -57,6 +111,7 @@ class BreakOutController extends Controller{
         $score = M('breakout');
         $data = array('phone' => $phone, 'use_time' => $useTime, 'barrier' => $barrier); //useTime 为所花费的时间 单位秒
         $score->add($data);
+        $this->getRank($useTime, $barrier);
     }
 
     private function getRank($useTime, $barrier){
@@ -78,5 +133,42 @@ class BreakOutController extends Controller{
             );
             $this->ajaxReturn($response);
         }
+    }
+
+    private function weChatAuth($code){
+        $authTokenUrlFinal = sprintf($this->authTokenUrl, $this->appId, $this->secret, $code, $this->grantType);
+        $resultJson = file_get_contents($authTokenUrlFinal);
+        $resultArr = json_decode($resultJson, true);
+        if(key_exists('errcode', $resultArr)){
+            echo '<script>alert("认证失败");</script>';
+            return;
+        }
+        $this->onAuthSuccess($code);
+    }
+
+    private function antiCheat(){
+        //取出I函数的值到变量，使empty兼容低版本php
+        $getCode = I('get.code');
+        $getState = I('get.state');
+        $authCode = I('session.code');
+        $authUrlFinal = $this->getAuthUrl();
+        if(!empty($getCode) && $this->state == $getState){
+            $this->weChatAuth($getCode);
+            return;
+        }
+        if(empty($authCode)){
+            header('Location:'.$authUrlFinal);
+            return;
+        }
+    }
+
+    private function onAuthSuccess($code){
+        session('code', $code);
+        header('Location:'.U('Home/BreakOut/index'));
+    }
+
+    private function getAuthUrl(){
+        $reUrlByUrlEncode = urlencode($this->redirectUrl);
+        return sprintf($this->authtUrl, $this->appId, $reUrlByUrlEncode, $this->responseType, $this->scope, $this->state);
     }
 }
