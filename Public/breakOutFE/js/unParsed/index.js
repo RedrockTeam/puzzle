287fb503feabbbee51b42a77c79f639f1d2a069b
Hangeer <591417089@qq.com>
2016-05-29 17:29:16 +0800

    Try to solve load problem

--- Public/breakOutFE/js/unParsed/index.js
+++ Public/breakOutFE/js/unParsed/index.js
@@ -1,3 +1,25 @@
+window.addEventListener('load', () => {
+    window.addEventListener('touchmove', (e) => {
+        e.preventDefault();
+    });
+    document.querySelector("#cover").addEventListener('touchmove', (e) => {
+        e.preventDefault();
+    });
+    document.querySelector("#cover").className += " cover-hide";
+    setTimeout(() => {
+        document.querySelector("#banner").className += " banner-bounce";
+        document.querySelector("#start").className += " start-after";
+        document.querySelector("#intro").className += " intro-after";
+        document.querySelector("#redrock").className += " redrock-after";
+    }, 1000);
+    document.querySelector("#start").addEventListener('touchstart', () => {
+        window.location.href = './game.html';
+    });
+    document.querySelector("#intro").addEventListener('touchstart', () => {
+        window.location.href = './intro.html';
+    });
+});
+/*
 (function () {
     window.addEventListener('touchmove', (e) => {
         e.preventDefault();
@@ -5,16 +27,15 @@
     document.querySelector("#cover").addEventListener('touchmove', (e) => {
         e.preventDefault();
     });
-
-    setTimeout(() => {
-        document.querySelector("#cover").className += " cover-hide";
-        setTimeout(() => {
-            document.querySelector("#banner").className += " banner-bounce";
-            document.querySelector("#start").className += " start-after";
-            document.querySelector("#intro").className += " intro-after";
-            document.querySelector("#redrock").className += " redrock-after";
-        }, 1000);
-    }, 5000);
+    // setTimeout(() => {
+    //     document.querySelector("#cover").className += " cover-hide";
+    //     setTimeout(() => {
+    //         document.querySelector("#banner").className += " banner-bounce";
+    //         document.querySelector("#start").className += " start-after";
+    //         document.querySelector("#intro").className += " intro-after";
+    //         document.querySelector("#redrock").className += " redrock-after";
+    //     }, 1000);
+    // }, 5000);
 
     //document.addEventListener('readystatechange', () => {
     //    if (document.readyState === "complete") {
@@ -27,8 +48,6 @@
     //        }, 1000);
     //    }
     //});
-    /* 安卓奇怪的问题, 不能判断加载完成 */
-
 
     document.querySelector("#start").addEventListener('touchstart', () => {
         window.location.href = './game.html';
@@ -37,3 +56,4 @@
         window.location.href = './intro.html';
     });
 })();
+*/
